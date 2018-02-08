6781f7629bfa4b5b8e3b3ddba1f3008b5d6ff468
danibelve <silvia.belvedere@mercadolibre.com>
2018-02-08 20:25:04 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,10 @@ function Mostrar()
 {
 //tomo la edad  
 
+    var edad = document.getElementById("edad").value;
 
+    if (edad == 15){
+        alert("Niña bonita");
+    }
 
 }//FIN DE LA FUNCIÓN
