b16b48678ab6dfd54352c25a1979d4a2866cdaf3
danibelve <silvia.belvedere@mercadolibre.com>
2018-02-19 19:57:26 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,20 @@
 function Mostrar()
 {
+	var contador = 0;
+	var acumulador = 0;
+	var numero;
+	var promedio;
+   
+	while (contador < 5) {
+	   numero = parseInt(prompt("Ingrese un número"));
+	   acumulador += numero;
+	   contador++;
+	}
 
-	var contador=0;
-	var acumulador=0;
-
+	promedio = acumulador / contador;
 
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('promedio').value=promedio;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,10 +3,20 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var numero;
+	var respuesta="si";
+	var promedio;
 
+	while(respuesta == "si") {
+		numero = parseInt(prompt('Ingresa el número'));
+		respuesta = prompt('¿seguir ingresando números?');
+		acumulador += numero;
+		contador++;
+	}
+
+	promedio = acumulador/contador;
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+document.getElementById('promedio').value=promedio;
 
 }//FIN DE LA FUNCIÓN
